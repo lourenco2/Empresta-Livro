1123a66be5033544e4fd338fd4b75d49fd27b7ae
AndreiReikdal <andeistachera@gmail.com>
2023-06-10 23:21:18 -0300

    style: Atualizando front deletando...

--- frontend/src/paginas/Montagem.jsx
+++
@@ -1,9 +0,0 @@
-function Montagem() {
-  return (
-    <div className="principal">
-      <h3>Conteúdo da página montagem</h3>
-    </div>
-  );
-}
-
-export default Montagem;
--- frontend/src/paginas/Util.jsx
+++
@@ -1,9 +0,0 @@
-function Util() {
-  return (
-    <div className="principal">
-      <h3>Conteúdo da página útil</h3>
-    </div>
-  );
-}
-
-export default Util;
